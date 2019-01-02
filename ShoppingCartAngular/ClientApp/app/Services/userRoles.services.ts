6741b50c00f76a2f8ab6d8aa3e0ca57ea0e8147e
pravinlalge <pravinlalge9@gmail.com>
2019-01-02 11:23:27 +0530

    Add project files.

---
+++ ShoppingCartAngular/ClientApp/app/Services/userRoles.services.ts
@@ -0,0 +1,27 @@
+﻿import { Injectable, Inject } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class UserRoleService {
+    myAppUrl: string = "http://localhost:57626/api/TestNew";
+
+    constructor(private _http: Http) {
+    }
+
+    saveUserRoles(userRole: any) {
+        debugger
+        return this._http.post(this.myAppUrl, userRole)
+            .map((response: Response) => response.json())
+            .catch(this.errorHandler)
+    }
+
+    errorHandler(error: Response) {
+        console.log(error);
+        return Observable.throw(error);
+    }
+}  
---
+++ ShoppingCartAngular/ClientApp/app/components/UserRole/UserRole.Component.ts
@@ -0,0 +1,43 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { NgForm, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { UserRoleService } from '../../Services/userRoles.services'
+ 
+
+@Component({
+    templateUrl: './UserRole.component.html'
+})
+
+export class UserRoleComponent {
+    userRoleForm: FormGroup;
+    roleId: number;
+    errorMessage: any;
+
+    constructor(private _fb: FormBuilder, private _avRoute: ActivatedRoute,
+        private _userRoleService: UserRoleService, private _router: Router) {
+        if (this._avRoute.snapshot.params["id"]) {
+            this.roleId = this._avRoute.snapshot.params["id"];
+        }
+
+        this.userRoleForm = this._fb.group({
+            roleId: 0,
+            roleName: ['', [Validators.required]],
+        })
+    }
+
+    saveUserRole() {
+        debugger
+        if (!this.userRoleForm.valid) {
+            return;
+        }
+        this._userRoleService.saveUserRoles(this.userRoleForm.value)
+                .subscribe((data) => {
+                   // this._router.navigate(['/fetch-employee']);
+                }, error => this.errorMessage = error)
+    }
+    cancel() {
+        //this._router.navigate(['/fetch-employee']);
+    }
+    get roleName() { return this.userRoleForm.get('roleName'); }
+}  
